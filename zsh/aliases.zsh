6332a9e5242674555b322caa775b5317cd25ce17
Mark Striemer <mark.striemer@canadadrugs.com>
2012-07-20 17:12:40 -0500

    Add bundler aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,5 +1,14 @@
 alias t="tmux -u"
 alias r="rails"
+
+# pip
 alias pi="pip install"
 alias pr="pip install -r requirements.txt"
 alias pf='pip freeze -r requirements.txt | grep -v "^#"'
+
+# bundler
+alias be="bundle exec"
+alias bi="bundle install"
+alias bl="bundle list"
+alias bp="bundle package"
+alias bu="bundle update"
